4eb27aa36309fb6f9bc0b75e1e226d9f781ffdc1
duongminh <minhduong18032019>
2022-08-26 22:43:47 +0700

    commit 3

--- app/Http/Controllers/Admin/Account/StaffAccount.php
+++ app/Http/Controllers/Admin/Account/StaffAccount.php
@@ -5,4 +5,8 @@ namespace App\Http\Controllers\Admin\Account;
 class StaffAccount
 {
 
+    public function func1()
+    {
+        echo 'function 1';
+    }
 }
--- app/Models/Admin/Account/Business.php
+++ app/Models/Admin/Account/Business.php
@@ -5,3 +5,13 @@
  * Date: 2022-08-26
  * Time: 22:20
  */
+
+namespace App\Http\Models\Admin\Account;
+
+class Business
+{
+    public function businessFunc1()
+    {
+        echo 'Business function 1';
+    }
+}
