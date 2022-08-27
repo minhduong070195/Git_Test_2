62a7e315875c4b856f8234715b6fe580857c0213
duongminh <minhduong18032019>
2022-08-27 09:37:59 +0700

    27/8-commit 1

--- app/Models/Admin/Account/Models/StaffAccountModel.php
+++ app/Models/Admin/Account/Models/StaffAccountModel.php
@@ -5,3 +5,12 @@
  * Date: 2022-08-26
  * Time: 22:20
  */
+namespace App\Models\Admin\Account\Models\StaffAccountModel;
+
+class StaffAccountModel
+{
+    public function staffModelFunc()
+    {
+        echo "staff model function 1";
+    }
+}
