ad0e7ccafe8213d8c997563a5ba590446eb0649e
duongminh <minhduong18032019>
2022-08-27 12:07:28 +0700

    27/8 - commit 9

---
+++ app/Models/Admin/Account/Models/StaffAccountCondition.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rinpt
+ * Date: 2022-08-26
+ * Time: 22:20
+ */
+namespace App\Models\Admin\Account\Models\StaffAccountModel;
+
+class StaffAccountCondition
+{
+    public function staffConditionFunc()
+    {
+        echo "staff condition function 1";
+    }
+}
