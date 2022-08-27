48a0af1aaa8d082d9e88f36e9473221e5e46b2cb
duongminh <minhduong18032019>
2022-08-27 16:14:53 +0700

    27/8 - commit 12

---
+++ app/Models/Admin/Account/Models/StaffAccountUpdateParams.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rinpt
+ * Date: 2022-08-26
+ * Time: 22:20
+ */
+namespace App\Models\Admin\Account\Models\StaffAccountModel;
+
+class StaffAccountUpdateParams
+{
+    public function staffUpdateParamFunc1()
+    {
+        echo "staff update param function 1";
+    }
+
+}
